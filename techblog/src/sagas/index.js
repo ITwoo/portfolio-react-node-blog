e0cf889536f7a58ec0cee99f4d1e6b7189e3413d
ITWoo <chldngur101@gmail.com>
2021-10-08 11:10:06 +0900

    add build folder

--- techblog/src/sagas/index.js
+++ techblog/src/sagas/index.js
@@ -4,7 +4,8 @@ import axios from "axios";
 import post from "./post";
 import user from './user'
 
-axios.defaults.baseURL = 'http://localhost:8080';
+// axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.baseURL = 'https://backserver-techblog.herokuapp.com/'
 axios.defaults.withCredentials = true;
 
 export default function* rootSaga() {
